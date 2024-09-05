5c50780c99bda8d578ed162036e96d67eb7f2201
muammarrezapahlawan <penareza@gmail.com>
2024-09-05 09:18:18 +0800

    add person class

---
+++ lesson01/oop/person.py
@@ -0,0 +1,30 @@
+
+
+# membuat class
+
+class Person:
+  name=""
+  address=""
+  
+  
+
+  # def getName(self):
+  #   return self.name
+  
+
+# membuat object
+person1 = Person()
+person2 = Person()
+
+
+
+person1.name="Reza"
+person1.address="tanukila"
+
+person2.name="Yaya"
+person2.address="Balaikota"
+
+
+print(person2.name)
+print(person1.address)
+
